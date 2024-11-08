f4a55702c7bfb569f5ddbc6649030d94d8c92a71
yahorlahunovich <egorlagunovich@gmail.com>
2024-11-08 18:19:44 +0100

    dictatorship

--- democracy-dictatorship/democracy.R
+++ democracy-dictatorship/democracy.R
@@ -1,2 +1,18 @@
+library(maps)
+library(mapdata)
+library(dplyr)
+library(ggplot2)
+
 tuesdata <- tidytuesdayR::tt_load('2024-11-05')
 democracy_data <- tuesdata$democracy_data
+
+str(democracy_data)
+
+belarus <- democracy_data[democracy_data$country_name == "Belarus", ]
+
+df <- democracy_data
+
+dictatorship <- df %>% 
+  filter(regime_category %in% c("Royal dictatorship",                                                       
+                                "Civilian dictatorship",                                                    
+                                "Military dictatorship" ))
