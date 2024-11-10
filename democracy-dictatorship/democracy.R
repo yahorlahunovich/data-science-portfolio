a5ae28b850bb6e3cbb98e83876d98beeda1ea1ae
yahorlahunovich <egorlagunovich@gmail.com>
2024-11-11 00:57:06 +0100

    finished dictatorship plot

--- democracy-dictatorship/democracy.R
+++ democracy-dictatorship/democracy.R
@@ -24,12 +24,6 @@ dictatorship <- df %>%
   group_by(country_name) %>% 
   summarise(count = n())
 
-
-world <- ne_countries(scale = "medium", returnclass = "sf")
-world_with_count <- world %>%
-  left_join(dictatorship, by = c("name" = "country_name")) %>% 
-  filter(name != "Antarctica")
-
 df <- df %>%
   mutate(country_name = case_when(
     country_name == "Bosnia and Herzegovina" ~ "Bosnia and Herz.",
@@ -48,14 +42,19 @@ df <- df %>%
     country_name == "Slovak Republic" ~ "Slovakia",
     country_name == "South Sudan" ~ "S. Sudan",
     country_name == "Swaziland" ~ "eSwatini",
-    TRUE ~ country_name # Keep other country names unchanged
+    TRUE ~ country_name
   ))
 
+world <- ne_countries(scale = "medium", returnclass = "sf")
+world_with_count <- world %>%
+  left_join(dictatorship, by = c("name" = "country_name")) %>% 
+  filter(name != "Antarctica")
 
 world_with_count <- world_with_count %>%
   mutate(
     count_category = case_when(
-      count == 0 ~ "0",
+      is.na(count) ~ "0 years",
+      count == 0 ~ "0 years",
       count > 0 & count <= 10 ~ "<10 years",
       count > 10 & count <= 30 ~ "10-30 years",
       count > 30 & count <= 50 ~ "30-50 years",
@@ -64,23 +63,27 @@ world_with_count <- world_with_count %>%
   )
 
 ggplot(data = world_with_count) +
-  geom_sf(aes(fill = count), color = "white") +
-  scale_fill_gradientn(
-    colors = c("lightgrey", "#FFD700", "#FFA500", "#FF4500", "#8B0000"),
-    na.value = "lightgrey",
-    guide = guide_colorbar(
-      title = "Years of Dictatorship",
-      title.position = "top",
-      title.hjust = 0.5,
-      direction = "vertical",
-      barwidth = 0.8,
-      barheight = 10
-    )
-  ) +
+  geom_sf(aes(fill = count_category), color = "white") +
   labs(
     title = "Global Dictatorship Trends by Country",
     subtitle = "Years of Dictatorship from 1950 to 2020",
     caption = "Made by Yahor Lahunovich\nData Source: [Add Source]"
+  ) +
+  scale_fill_manual(
+    values = c(
+      "<10 years" = "#FFD700",
+      "0 years" = "lightgrey",
+      "10-30 years" = "#FFA500",
+      "30-50 years" = "#FF4500",
+      "50+ years" = "#8B0000"
+    ),
+    guide = guide_legend(
+      title = "Years of Dictatorship",
+      title.position = "top",
+      title.hjust = 0.5,
+      direction = "vertical",
+      override.aes = list(size = 6)
+    )
   ) +
   theme_minimal(base_size = 14) +
   theme(
@@ -88,18 +91,18 @@ ggplot(data = world_with_count) +
     plot.title = element_text(color = "black", hjust = 0.5, size = 29, face = "bold"),
     plot.subtitle = element_text(color = "darkgrey", hjust = 0.5, size = 14, face = "italic"),
     plot.caption = element_text(color = "grey40", face = "italic", size = 10, hjust = 1),
-    legend.position = c(0.2, 0.3), 
+    legend.position = c(0.15, 0.35), 
     legend.title = element_text(face = "bold"),
     legend.text = element_text(size = 10),
-    legend.key.size = unit(0.8, "cm"), 
-    legend.spacing.y = unit(0.2, "cm"), 
     axis.text = element_blank(),
     axis.ticks = element_blank(),
     panel.border = element_blank()
   ) +
   coord_sf(crs = "+proj=robin")
 
-  
+
+
+
 # democracy
 
 democracy <- df %>% 
